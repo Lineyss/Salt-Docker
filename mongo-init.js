053b429f782fc3ad835fe0abfb24c31a9e7116cd
Lineyss <pikachu.ionenko@gmail.com>
2024-10-24 14:38:38 +0300

    Update mongo container

--- mongo-init.js
+++ mongo-init.js
@@ -1,7 +1,12 @@
-db = db.getSiblingDB('salt_db');
-
-db.createUser({
-    user: "salt",
-    pwd: "123321",
-    roles: [{ role: "readWrite", db: "salt_db" }]
-});
+db.createUser(
+    {
+        user: "salt",
+        pwd: "123321",
+        roles: [
+            {
+                role: "readWrite",
+                db: "salt_db"
+            }
+        ]
+    }
+);
